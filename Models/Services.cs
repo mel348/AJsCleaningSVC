5535fb534f57c2f03830c47cb466ff907cc88a06
scottwesenberg <scowesen@gmail.com>
2024-02-22 19:59:00 -0500

    Add project files.

---
+++ Models/Services.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace AJsCleaning.Models {
+    public enum PropertyType {
+        Residential,
+        Commercial,
+        Other
+    }
+
+    public class Services {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public PropertyType Type { get; set; }
+        [Required]
+        public string Service { get; set; } = String.Empty;
+        [Required]
+        public string Description { get; set; } = String.Empty;
+    }
+}
