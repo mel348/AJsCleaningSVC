d21d54ed9a57b390442897d773b9423b2881eff7
Nicole <wolfn@mail.nmc.edu>
2024-02-22 23:33:57 -0500

    Added gallery

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace AJsCleaning.Controllers
         {
             return View();
         }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
