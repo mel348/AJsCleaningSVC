83561d6a390c735b08c39ebb28b24683192e9319
scottwesenberg <scowesen@gmail.com>
2024-02-27 22:18:57 -0500

    Services page, site.js, site.css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(function () {
+    // Hide all service rows initially
+    $('tr.service-row').hide();
+
+    // Handle click on type row to toggle service rows
+    $('tr.type-row').on('click', function () {
+        $(this).nextUntil('tr.type-row').toggle();
+    });
+});
