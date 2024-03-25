a7a2a345f6907e834d8afded28eaf7615597b500
Nicole <wolfn@mail.nmc.edu>
2024-03-25 14:45:31 -0400

    Created DbSet and CRUD functionality for Team

--- Migrations/20240325174533_PhotoUploadTeam.Designer.cs
+++ Migrations/20240325174533_PhotoUploadTeam.Designer.cs
@@ -3,6 +3,7 @@ using AJsCleaning.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AJsCleaning.Migrations
 {
     [DbContext(typeof(AJContext))]
-    partial class AJContextModelSnapshot : ModelSnapshot
+    [Migration("20240325174533_PhotoUploadTeam")]
+    partial class PhotoUploadTeam
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
